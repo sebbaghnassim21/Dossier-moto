aea9cefcad448152cfb3488a52a1b02ab6abb45e
frappe <frappe@ubuntu.vm>
2022-08-25 08:10:08 +0000

    first

---
+++ dossier/dossier/doctype/dossier_moto/test_dossier_moto.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2022, nassim and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+# import frappe
+import unittest
+
+class TestDossierMoto(unittest.TestCase):
+	pass
